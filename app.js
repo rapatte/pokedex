36a6432f3fa7e3672d7c75f34d583f766c81ea90
antoine <antoine.gayoso@gmail.com>
2021-02-16 16:44:09 +0100

    pouet

--- app.js
+++ app.js
@@ -13,7 +13,12 @@ var rightContainerScreen = document.querySelector(".right-container__screen");
 var listItem = document.querySelectorAll(".list-item")
 var leftButton = document.querySelector('.left-button');
 var rightButton = document.querySelector('.right-button');
+var flecheHaut = document.querySelector('d-pad__cell top');
 
+var listSuivante;
+var listprecedente;
+var id;
+var url;
 
 
 
@@ -21,10 +26,11 @@ function getPokemon(url) {
     fetch(url)
         .then(pouet => pouet.json())
         .then(data => {
-
+            listPrecedente = data.previous;
+            listSuivante = data.next;
             var results = data.results;
 
-            console.log(data);
+            
             
 
 
@@ -33,18 +39,78 @@ function getPokemon(url) {
                 var listPokedex = listItem[i];
                 var listPokemon = results[i];
 
-                var url = listPokemon.url;
+                url = listPokemon.url;
                 var name = listPokemon.name;
 
                 //récupération de l'id dans l'url
-                var id = url.split('/')[url.split('/').length - 2];
+                const id = url.split('/')[url.split('/').length - 2];
 
-                console.log(id);
                 
-                listPokedex.textContent = `${id}. ${name}`
+
+                listPokedex.textContent = `${id}. ${name}`;
+                
+                
 
             }
+            
+            
         })
         
+
 }
+
+function getPokeData (id){
+    fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+    .then(pouet => pouet.json())
+    .then(data => {
+        // console.log(data.types[0].type.name);
+        pokeTypeOne.textContent = data.types[0].type.name;
+        if(data.types[1]){
+            pokeTypeTwo.textContent = data.types[1].type.name;
+        }
+        else {
+            pokeTypeTwo.classList.add('hide');
+        }
+        pokeName.textContent = data.name;
+        pokeId.textContent = '# ' + data.id;
+        mainScreen.setAttribute('class', 'main-screen ' + data.types[0].type.name);
+        pokeBackImage.src = data.sprites.back_default;
+        pokeFrontImage.src = data.sprites.front_default;
+        pokeWeight.textContent = data.weight;
+        pokeHeight.textContent = data.height;
+
+
+        
+        
+    })
+}
+
+
+
+mainScreen.classList.remove('hide');
+
+
+
+rightButton.addEventListener('click', function() {if(listSuivante)getPokemon(listSuivante)});
+
+leftButton.addEventListener('click', function() {if(listPrecedente)getPokemon(listPrecedente)});
+
+
+
+
 getPokemon('https://pokeapi.co/api/v2/pokemon?offset=0&limit=20');
+
+
+
+function afficherData (){
+    let liste = document.querySelectorAll('.list-item')
+    for (let i = 0; i < liste.length; i++) {
+        let element = liste[i];
+        element.addEventListener('click',function (){
+            console.log(element);
+            let id = element.textContent.split('.')[0];
+            getPokeData(id) 
+        })        
+    }
+}
+afficherData();
